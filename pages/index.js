69425df3b8a63f06855de510e207cce3a11db03f
Melihcan Çilek <mcilek2018@gtu.edu.tr>
2023-02-11 13:36:10 +0300

    build

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,6 @@ const Home = () => {
       <div style={styles.btnContainer}>
         <a style={styles.btnInner} href="/addUser">Add User</a>
         <a style={styles.btnInner} href="/getClips">Get Clips</a>
-        asdsa
       </div>
     </div>
   )
