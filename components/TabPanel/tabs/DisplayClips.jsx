01a5557fb73fc2de7902c211757fd6aec81b84f1
Melihcan Çilek <mcilek2018@gtu.edu.tr>
2023-02-26 11:06:58 +0300

    !!!!!!!!!!!!!!!!!!!!!!!!! clip download done !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- components/TabPanel/tabs/DisplayClips.jsx
+++ components/TabPanel/tabs/DisplayClips.jsx
@@ -16,6 +16,20 @@ const DisplayClips = ({ clips }) => {
                         <div style={styles.center}>
                             <p>Number of views: {clip.view_count}</p>
                         </div>
+                        <div style={styles.center}>
+                            <button style={styles.btn} onClick={() => {
+                                navigator.clipboard.writeText(`https://clips.twitch.tv/${clip.id}`).then(() => {
+                                    alert('Copied to clipboard')
+                                })
+                            }}>Copy link</button>
+                            <button style={styles.btn} onClick={() => {
+                                // download video clip
+                                const a = document.createElement('a');
+                                a.href = clip.thumbnail_url.replace('-preview-480x272.jpg', '.mp4');
+                                a.download = clip.title;
+                                a.click();
+                            }}>Download</button>
+                        </div>
                     </div >
                 ))
 
