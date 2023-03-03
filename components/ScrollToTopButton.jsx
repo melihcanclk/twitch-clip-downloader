15ebdeb331df649be19cf49709a13bfd7e077da4
Melihcan Çilek <mcilek2018@gtu.edu.tr>
2023-03-03 15:52:31 +0300

    scroll to top style

--- components/ScrollToTopButton.jsx
+++ components/ScrollToTopButton.jsx
@@ -9,15 +9,28 @@ export const ScrollToTopButton = () => {
     return (
         <div>
             <button
-                style={{
-                    position: 'fixed',
-                    bottom: '1rem',
-                    right: '1rem',
-                    zIndex: 1000,
-                }}
+                className="scroll-to-top-button"
                 onClick={goToHeader}
             >
-                Scroll to top
+                <div 
+                    className="spinner-container width-100 height-100"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="feather feather-arrow-up"
+                    >
+                        <line x1="12" y1="19" x2="12" y2="5"></line>
+                        <polyline points="5 12 12 5 19 12"></polyline>
+                    </svg>
+                </div>
             </button>
         </div>
     );
