f11dc1d3da92305a3160cbdbe77b4a10ac3f08bd
Melihcan Cilek <melihcanclk@hotmail.com>
2023-06-28 23:08:04 +0300

    cliplength and error change

--- components/displayClips/DisplayError.jsx
+++ components/displayClips/DisplayError.jsx
@@ -2,39 +2,10 @@ import React from 'react';
 import DisplayClips from '@/components/displayClips/DisplayClips';
 
 export const DisplayError = ({ clips, value, loading, error }) => {
+    let clipLength = Object.keys(clips).map((streamer) => clips[streamer].length).reduce((a, b) => a + b, 0);
+    console.log(clipLength)
     return (
-        <div >
-            {
-                loading ?
-                    <div className="spinner-container">
-                        <div className="loading-spinner">
-                        </div>
-                    </div>
-                    :
-                    (
-                        error ?
-                            <div
-                                className='error-text'
-                                style={{
-                                    color: 'red',
-                                    fontSize: '1rem',
-                                    fontWeight: 'bold',
-                                    textAlign: 'center',
-                                }}
-                            >
-                                Error while loading clips
-                            </div>
-                            :
-                            Object.keys(clips).length > 0 ?
-                                <DisplayClips value={value} clips={clips} />
-                                :
-                                <div style={{
-                                    width: '100%',
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                }}>No clips found</div>
-                    )
-            }
-        </div>
-    )
+        clipLength > 0 &&
+            <DisplayClips clips={clips} value={value} loading={loading} />
+    );
 }
