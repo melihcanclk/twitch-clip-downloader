e0c25976ac9fee899b698c2ae8f575bf843b9bfd
Melihcan Cilek <melihcanclk@hotmail.com>
2023-06-28 23:12:21 +0300

    deploy console.log

--- components/getClips/GetClips.jsx
+++ components/getClips/GetClips.jsx
@@ -31,6 +31,7 @@ export const GetClips = ({ clips, loading, error, day, numberOfClips, setClips, 
                 try {
                     const { data } = await fetchData(`https://api.twitch.tv/helix/clips?broadcaster_id=${userID}&first=${numberOfClips}&started_at=${dayBeforeISO}`);
                     // filter clips that are not from valorant
+                    console.log({ data })
                     if (data.length > 0) {
                         const filteredClips = data.filter(clip => clip.game_id === game_id).slice(0, 3);
                         console.log(filteredClips)
