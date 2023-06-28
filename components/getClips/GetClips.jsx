26f563e6c23711f8624a69e54560ee775ced6452
Melihcan Cilek <melihcanclk@hotmail.com>
2023-06-28 23:39:42 +0300

    getClips functionm moved to useffect

--- components/getClips/GetClips.jsx
+++ components/getClips/GetClips.jsx
@@ -9,48 +9,48 @@ export const GetClips = ({ clips, loading, error, day, numberOfClips, setClips, 
     // get users from firebase
     const [streamerBuffer, setStreamerBuffer] = React.useState([]);
 
-    async function getClips() {
-        setLoading(true);
-        setError(false);
-        const BUFFER_SIZE = 100;
-        for (let i = 0; i < streamers.length; i += BUFFER_SIZE) {
-            // buffer.push(streamers.slice(i, i + BUFFER_SIZE));
-            setStreamerBuffer((prev) => [...prev, streamers.slice(i, i + BUFFER_SIZE)]);
-        }
-        console.log({ streamerBuffer })
-        // get clips for each streamer in buffer
-        for (let j = 0; j < streamerBuffer.length; j++) {
-            for (let k = 0; k < streamerBuffer[j].length; k++) {
-                const username = (type === TypeOfClip.FIREBASE ? streamerBuffer[j][k].username : streamerBuffer[j][k].to_name);
-                const userID = await convertUserNameToID(username);
-                const game = await fetchData(`https://api.twitch.tv/helix/games?name=Valorant`)
-                const game_id = game.data[0].id;
-                const today = new Date();
-                const dayBefore = new Date(today.getFullYear(), today.getMonth(), today.getDate() - day);
-                const dayBeforeISO = dayBefore.toISOString();
-                // TODO : add pagination
-                try {
-                    const { data } = await fetchData(`https://api.twitch.tv/helix/clips?broadcaster_id=${userID}&first=${numberOfClips}&started_at=${dayBeforeISO}`);
-                    console.log({ data })
-                    // filter clips that are not from valorant
-                    if (data.length > 0) {
-                        const filteredClips = data.filter(clip => clip.game_id === game_id).slice(0, 3);
-                        // setClips with username as key and clips as value
-                        setClips((prev) => ({ ...prev, [username]: filteredClips }));
-                    }
-                    setLoading(false);
-                }
-                catch (err) {
-                    setError(true);
-                    console.log(err);
-                }
-            }
-        }
-    }
+
 
     useEffect(() => {
         // when value changes, get clips using entry username
-        console.log({ streamers })
+        async function getClips() {
+            setLoading(true);
+            setError(false);
+            const BUFFER_SIZE = 100;
+            for (let i = 0; i < streamers.length; i += BUFFER_SIZE) {
+                // buffer.push(streamers.slice(i, i + BUFFER_SIZE));
+                setStreamerBuffer((prev) => [...prev, streamers.slice(i, i + BUFFER_SIZE)]);
+            }
+            console.log({ streamerBuffer })
+            // get clips for each streamer in buffer
+            for (let j = 0; j < streamerBuffer.length; j++) {
+                for (let k = 0; k < streamerBuffer[j].length; k++) {
+                    const username = (type === TypeOfClip.FIREBASE ? streamerBuffer[j][k].username : streamerBuffer[j][k].to_name);
+                    const userID = await convertUserNameToID(username);
+                    const game = await fetchData(`https://api.twitch.tv/helix/games?name=Valorant`)
+                    const game_id = game.data[0].id;
+                    const today = new Date();
+                    const dayBefore = new Date(today.getFullYear(), today.getMonth(), today.getDate() - day);
+                    const dayBeforeISO = dayBefore.toISOString();
+                    // TODO : add pagination
+                    try {
+                        const { data } = await fetchData(`https://api.twitch.tv/helix/clips?broadcaster_id=${userID}&first=${numberOfClips}&started_at=${dayBeforeISO}`);
+                        console.log({ data })
+                        // filter clips that are not from valorant
+                        if (data.length > 0) {
+                            const filteredClips = data.filter(clip => clip.game_id === game_id).slice(0, 3);
+                            // setClips with username as key and clips as value
+                            setClips((prev) => ({ ...prev, [username]: filteredClips }));
+                        }
+                        setLoading(false);
+                    }
+                    catch (err) {
+                        setError(true);
+                        console.log(err);
+                    }
+                }
+            }
+        }
         if (streamers.length > 0) {
             getClips();
         }
