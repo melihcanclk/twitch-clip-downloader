16023aa06c6cc6e007c8f12a39f684727bbd5d0b
Melihcan Çilek <mcilek2018@gtu.edu.tr>
2023-08-20 14:43:58 +0300

    Update index.js
    update in get clips

--- pages/getClips/index.js
+++ pages/getClips/index.js
@@ -8,7 +8,7 @@ const GetClipsIndex = () => {
             <div style={styles.body} className='spinner-container height-100' >
                 {/* <a style={styles.btnInner} href={`/getClips/search`}>Search</a> */}
                 {/* <a style={styles.btnInner} href={`/getClips/firebase`}>Firebase</a> */}
-                <a style={styles.btnInner} href={`/getClips/twitch-api`}>Twitch API</a>
+                {/* <a style={styles.btnInner} href={`/getClips/twitch-api`}>Twitch API</a> */}
                 <a style={styles.btnInner} href={`/getClips/following`}>Following</a>
             </div>
         </ScrollToTopComponent>
